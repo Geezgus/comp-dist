684a6964077d268fd69e4232feffc45ad7d37455
Gustavo Duarte <gutorap.gh@gmail.com>
2024-11-06 10:43:28 -0300

    working version. Note: image files are reference, only

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -2,13 +2,13 @@ from locust import HttpUser, TaskSet, task, between
 
 
 class UserTasks(TaskSet):
-    @task
-    def img_large(self):
-        self.client.get("/large-image")
-
-    @task
-    def img_medium(self):
-        self.client.get("/medium-image")
+#    @task
+#    def img_large(self):
+#        self.client.get("/large-image")
+#
+#    @task
+#    def img_medium(self):
+#        self.client.get("/medium-image")
 
     @task
     def txt_large(self):
